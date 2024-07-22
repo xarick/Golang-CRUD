d10f859151f5fc0b1d3c00082d8c13433c1eab10
Bobirjon Tohirov <tokhirovbob@gmail.com>
2024-07-22 16:42:24 +0500

    added files main and app

--- internal/bootstrap/app.go
+++ internal/bootstrap/app.go
@@ -1,1 +1,54 @@
 package bootstrap
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/xarick/golang-crud/config"
+	v1 "github.com/xarick/golang-crud/internal/gateways/rest/v1"
+	"github.com/xarick/golang-crud/internal/services"
+)
+
+type App struct {
+	cfg    config.Application
+	engine *gin.Engine
+}
+
+func New(cfg config.Application) *App {
+	service := services.NewService(&cfg, CreateHTTPSClient())
+	ctrl := v1.NewController(&cfg, service)
+
+	r := gin.Default()
+	engine := v1.NewRouter(r, ctrl)
+
+	app := App{
+		cfg:    cfg,
+		engine: engine,
+	}
+
+	return &app
+}
+
+func (app *App) Run(ctx context.Context, cfg config.Application) {
+	go func() {
+		err := app.engine.Run(cfg.RunPort)
+		if err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	<-ctx.Done()
+}
+
+func CreateHTTPSClient() *http.Client {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	return &http.Client{Transport: tr}
+}
