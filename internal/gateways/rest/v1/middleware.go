34b1f80325c3ceb6b4be0a3c89e5dc1a540c3ee0
Bobirjon Tohirov <tokhirovbob@gmail.com>
2024-07-22 16:13:41 +0500

    added middleware functions

--- internal/gateways/rest/v1/middleware.go
+++ internal/gateways/rest/v1/middleware.go
@@ -1,1 +1,31 @@
 package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/xarick/golang-crud/pkg"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		headerAuth := ctx.GetHeader("Authorization")
+		if len(headerAuth) == 0 {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"code": -101, "error": "login or password empty"})
+			return
+		}
+
+		login, password, err := pkg.BasicAuthLogPass(headerAuth)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"code": -102, "error": err.Error()})
+			return
+		}
+
+		if login == "admin" && password == "12345" {
+			ctx.Next()
+		} else {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"code": -103, "error": "login or password incorrect"})
+			return
+		}
+	}
+}
---
+++ pkg/base64.go
@@ -0,0 +1,23 @@
+package pkg
+
+import (
+	"encoding/base64"
+	"errors"
+	"strings"
+)
+
+func BasicAuthLogPass(basicAuth string) (login string, password string, err error) {
+	base64Data := basicAuth[6:]
+	decodedByte, err := base64.StdEncoding.DecodeString(base64Data)
+	if err != nil {
+		err = errors.New("error receiving login and password")
+		return
+	}
+
+	decodedString := string(decodedByte)
+	logAndPas := strings.Split(decodedString, ":")
+
+	login = logAndPas[0]
+	password = logAndPas[1]
+	return
+}
